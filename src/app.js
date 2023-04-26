574256a574ba2ee15acc4ff9dc0adfd4eba287b4
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-04-26 20:46:43 +0200

    (feat): Add origin to cors

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
