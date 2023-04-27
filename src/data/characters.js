c083c2df27a61497a87aa64811720803c2dcb4b8
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-04-27 10:03:29 +0200

    (feat): Add character data table

---
+++ src/data/characters.js
@@ -0,0 +1,8 @@
+exports.characters = [
+  {
+    name: "Character 1",
+  },
+  {
+    name: "Character 2",
+  },
+];
--- src/database/seedDB.js
+++ src/database/seedDB.js
@@ -2,9 +2,11 @@ const bcrypt = require("bcrypt");
 const { sequelize } = require("./config");
 
 const { items } = require("../data/items");
+const { characters } = require("../data/characters");
 
 const gameDb = async () => {
   try {
+    // Remove item table if it exists
     await sequelize.query(`DROP TABLE IF EXISTS item;`);
 
     // Create item table
@@ -35,6 +37,41 @@ const gameDb = async () => {
     itemInsertQuery += ";";
     await sequelize.query(itemInsertQuery, {
       bind: itemInsertQueryVariables,
+    });
+
+    // Remove character table if it exists
+    await sequelize.query(`DROP TABLE IF EXISTS character;`);
+
+    // Create character table
+    await sequelize.query(`
+    CREATE TABLE IF NOT EXISTS character (
+      character_id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL
+      );`);
+
+    let characterInsertQuery = "INSERT INTO character (name) VALUES ";
+
+    let characterInsertQueryVariables = [];
+
+    characters.forEach((character, index, array) => {
+      const variables = [character.name];
+      let string = "(";
+
+      for (let i = 1; i < variables.length + 1; i++) {
+        string += `$${characterInsertQueryVariables.length + i}`;
+        if (i < variables.length) string += ",";
+      }
+      characterInsertQuery += string + ")";
+      if (index < array.length - 1) characterInsertQuery += ",";
+
+      characterInsertQueryVariables = [
+        ...characterInsertQueryVariables,
+        ...variables,
+      ];
+    });
+    characterInsertQuery += ";";
+    await sequelize.query(characterInsertQuery, {
+      bind: characterInsertQueryVariables,
     });
 
     console.log("Database successfully populated with data...");
