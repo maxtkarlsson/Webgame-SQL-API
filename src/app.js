9a936cd55aec02b7d527a1d799a2adc55439a5c4
maxtkarlsson <113439377+maxtkarlsson@users.noreply.github.com>
2023-10-03 21:19:51 +0200

    (feat): Add loclal address to cors

--- src/app.js
+++ src/app.js
@@ -23,7 +23,11 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
